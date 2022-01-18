dc8f1f71810af740eabec68f29d5e91f8769f8ac
Squidman95 <squidmanse@gmail.com>
2022-01-18 15:19:48 +0100

    Equals and hashcode to enable assertEquals between events

--- src/main/java/messaging/EventResponse.java
+++ src/main/java/messaging/EventResponse.java
@@ -1,4 +1,6 @@
 package messaging;
+import java.util.Arrays;
+
 import com.google.gson.Gson;
 
 public class EventResponse {
@@ -47,7 +49,44 @@ public class EventResponse {
         this.arguments = arguments;
     }
 
-    public <T> T getArgument(int i, Class<T> cls) {
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.deepHashCode(arguments);
+		result = prime * result + ((errorMessage == null) ? 0 : errorMessage.hashCode());
+		result = prime * result + ((sessionId == null) ? 0 : sessionId.hashCode());
+		result = prime * result + (success ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EventResponse other = (EventResponse) obj;
+		if (!Arrays.deepEquals(arguments, other.arguments))
+			return false;
+		if (errorMessage == null) {
+			if (other.errorMessage != null)
+				return false;
+		} else if (!errorMessage.equals(other.errorMessage))
+			return false;
+		if (sessionId == null) {
+			if (other.sessionId != null)
+				return false;
+		} else if (!sessionId.equals(other.sessionId))
+			return false;
+		if (success != other.success)
+			return false;
+		return true;
+	}
+
+	public <T> T getArgument(int i, Class<T> cls) {
         // The hack is needed because of Events are converted
         // to JSon for transport. Because JSon does not store
         // the class of an Object, when deserializing the arguments
